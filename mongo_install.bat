64a498439ad1d5cb828e90548a64b62056ea3d8a
Oscar Fraxedas <fraxedas@gmail.com>
2015-12-28 12:55:44 -0500

    Update the mongo version

--- mongo_install.bat
+++ mongo_install.bat
@@ -4,7 +4,7 @@ REM Pre
 REM bitsadmin /transfer mydownloadjob  /download /priority normal %MONGO_URL% %MONGO_INSTALLER%
 
 REM Act
-msiexec.exe /quiet /i %MONGO_INSTALLER_PATH% INSTALLLOCATION="%MONGO_DIR%"
+msiexec.exe /quiet /i %MONGO_INSTALLER_PATH% INSTALLLOCATION="%MONGO_DIR%" ADDLOCAL="all"
 
 REM Post
 service_create
